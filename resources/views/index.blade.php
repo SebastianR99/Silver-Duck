d13fbc2bf99fec834faf08613baab9019cb30d6d
Sebastián Rubio <sebastian.rubio12@hotmail.com>
2021-02-06 11:40:12 -0500

    En el index Coloqué una imágen y rellené la sección de noticias

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1587,24 +1587,23 @@
       <div class="row">
         <div class="col-md-12">
           <div class="aa-latest-blog-area">
-            <h2>LATEST BLOG</h2>
+            <h2>ÚLTIMAS NOTICIAS</h2>
             <div class="row">
               <!-- single latest blog -->
               <div class="col-md-4 col-sm-4">
                 <div class="aa-latest-blog-single">
                   <figure class="aa-blog-img">                    
-                    <a href="#"><img src=" {{ url('/assets/img/promo-banner-1.jpg') }}" alt="img"></a>  
+                    <a href="https://fashionunited.es/noticias/moda/black-lives-matter-in-italian-fashion-abre-los-desfiles-de-milan/2021020534615"><img src=" {{ url('/assets/img/promo-banner-1.jpg') }}" alt="img"></a>  
                       <figcaption class="aa-blog-img-caption">
-                      <span href="#"><i class="fa fa-eye"></i>5K</span>
-                      <a href="#"><i class="fa fa-thumbs-o-up"></i>426</a>
-                      <a href="#"><i class="fa fa-comment-o"></i>20</a>
-                      <span href="#"><i class="fa fa-clock-o"></i>June 26, 2016</span>
+                      <span href="#"><i class="fa fa-clock-o"></i>Febrero 05, 2021</span>
                     </figcaption>                          
                   </figure>
                   <div class="aa-blog-info">
-                    <h3 class="aa-blog-title"><a href="#">Lorem ipsum dolor sit amet</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, ad? Autem quos natus nisi aperiam, beatae, fugiat odit vel impedit dicta enim repellendus animi. Expedita quas reprehenderit incidunt, voluptates corporis.</p> 
-                    <a href="#" class="aa-read-mor-btn">Read more <span class="fa fa-long-arrow-right"></span></a>
+                    <h3 class="aa-blog-title"><a href="#">Black Lives Matter in Italian Fashion abre los desfiles de Milán</a></h3>
+                    <p>Milán- Cinco estilistas del colectivo Black Lives Matter in Italian Fashion abrirán los desfiles de la 
+                      Semana de la Moda femenina de Milán, que inician el 23 de febrero, un evento en gran parte telemático 
+                      debido a la pandemia.</p> 
+                    <a href="https://fashionunited.es/noticias/moda/black-lives-matter-in-italian-fashion-abre-los-desfiles-de-milan/2021020534615" class="aa-read-mor-btn"> Saber más <span class="fa fa-long-arrow-right"></span></a>
                   </div>
                 </div>
               </div>
@@ -1612,18 +1611,17 @@
               <div class="col-md-4 col-sm-4">
                 <div class="aa-latest-blog-single">
                   <figure class="aa-blog-img">                    
-                    <a href="#"><img src=" {{ url('/assets/img/promo-banner-3.jpg') }}" alt="img"></a>  
+                    <a href="https://fashionunited.es/noticias/moda/la-mayoria-de-los-grandes-disenadores-ausentes-de-la-semana-de-la-moda-de-nueva-york/2021020434597"><img src=" {{ url('/assets/img/promo-banner-2.jpg') }}" alt="img"></a>  
                       <figcaption class="aa-blog-img-caption">
-                      <span href="#"><i class="fa fa-eye"></i>5K</span>
-                      <a href="#"><i class="fa fa-thumbs-o-up"></i>426</a>
-                      <a href="#"><i class="fa fa-comment-o"></i>20</a>
-                      <span href="#"><i class="fa fa-clock-o"></i>June 26, 2016</span>
+                      <span href="#"><i class="fa fa-clock-o"></i>Febrero 04, 2021</span>
                     </figcaption>                          
                   </figure>
                   <div class="aa-blog-info">
-                    <h3 class="aa-blog-title"><a href="#">Lorem ipsum dolor sit amet</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, ad? Autem quos natus nisi aperiam, beatae, fugiat odit vel impedit dicta enim repellendus animi. Expedita quas reprehenderit incidunt, voluptates corporis.</p> 
-                     <a href="#" class="aa-read-mor-btn">Read more <span class="fa fa-long-arrow-right"></span></a>         
+                    <h3 class="aa-blog-title"><a href="#">La mayoría de los grandes diseñadores ausentes de la Semana de la Moda de Nueva York</a></h3>
+                    <p>La mayoría de los grandes diseñadores que suelen participar en la Semana de la Moda de Nueva York 
+                      estarán ausentes este año: algunos presentarán su colección fuera del calendario, mientras otros no han 
+                      anunciado nada todavía. En la NYFW, que tendrá lugar del 14 al 17 de febrero.</p> 
+                     <a href="https://fashionunited.es/noticias/moda/la-mayoria-de-los-grandes-disenadores-ausentes-de-la-semana-de-la-moda-de-nueva-york/2021020434597" class="aa-read-mor-btn"> Saber más <span class="fa fa-long-arrow-right"></span></a>         
                   </div>
                 </div>
               </div>
@@ -1631,18 +1629,18 @@
               <div class="col-md-4 col-sm-4">
                 <div class="aa-latest-blog-single">
                   <figure class="aa-blog-img">                    
-                    <a href="#"><img src=" {{ url('/assets/img/promo-banner-1.jpg') }}" alt="img"></a>  
+                    <a href="https://fashionunited.es/noticias/moda/lo-mas-destacado-de-la-semana-de-la-moda-masculina-de-paris-fw21/2021020334587"><img src=" {{ url('/assets/img/promo-banner-3.jpg') }}" alt="img"></a>  
                       <figcaption class="aa-blog-img-caption">
-                      <span href="#"><i class="fa fa-eye"></i>5K</span>
-                      <a href="#"><i class="fa fa-thumbs-o-up"></i>426</a>
-                      <a href="#"><i class="fa fa-comment-o"></i>20</a>
-                      <span href="#"><i class="fa fa-clock-o"></i>June 26, 2016</span>
+                      <span href="#"><i class="fa fa-clock-o"></i>Febrero 03, 2021</span>
                     </figcaption>                          
                   </figure>
                   <div class="aa-blog-info">
-                    <h3 class="aa-blog-title"><a href="#">Lorem ipsum dolor sit amet</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, ad? Autem quos natus nisi aperiam, beatae, fugiat odit vel impedit dicta enim repellendus animi. Expedita quas reprehenderit incidunt, voluptates corporis.</p> 
-                    <a href="#" class="aa-read-mor-btn">Read more <span class="fa fa-long-arrow-right"></span></a>
+                    <h3 class="aa-blog-title"><a href="#">Lo más destacado de la Semana de la Moda masculina de París FW21</a></h3>
+                    <p>Trendstop ofrece a los lectores de FashionUnited las colecciones más destacadas vistas en la Semana
+                       de la Moda masculina de París, para Otoño/Invierno 2021-22. En los desfiles de la Semana de la Moda 
+                       de París vimos muchas influencias de la cultura el arte, los diseñadores rindieron homenaje a los 
+                       artistas que admiran o con los que han colaborado de alguna manera en sus proyectos. </p> 
+                    <a href="https://fashionunited.es/noticias/moda/lo-mas-destacado-de-la-semana-de-la-moda-masculina-de-paris-fw21/2021020334587" class="aa-read-mor-btn"> Saber más <span class="fa fa-long-arrow-right"></span></a>
                   </div>
                 </div>
               </div>
@@ -1653,32 +1651,6 @@
     </div>
   </section>
   <!-- / Latest Blog -->
-
-  <!-- Client Brand -->
-  <section id="aa-client-brand">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="aa-client-brand-area">
-            <ul class="aa-client-brand-slider">
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-java.png') }}" alt="java img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-jquery.png') }}" alt="jquery img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-html5.png') }}" alt="html5 img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-css3.png') }}" alt="css3 img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-wordpress.png') }}" alt="wordPress img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-joomla.png') }}" alt="joomla img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-java.png') }}" alt="java img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-jquery.png') }}" alt="jquery img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-html5.png') }}" alt="html5 img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-css3.png') }}" alt="css3 img"></a></li>
-              <li><a href="#"><img src=" {{ url('/assets/img/client-brand-wordpress.png') }}" alt="wordPress img"></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- / Client Brand -->
 
   
   <!-- footer -->  
